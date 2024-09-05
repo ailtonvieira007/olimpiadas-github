5802b0e0da6a8c1da2257a2082e8dd22ee84ec6f
Ailton Vieira <91479065+ailtonvieira007@users.noreply.github.com>
2024-09-05 15:57:07 -0300

    incluindo novas atletas

--- dados.js
+++ dados.js
@@ -13,5 +13,10 @@ let dados = [
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
-    }
+    },
+    {
+    titulo: "Tatiana Weston-Webb",
+    descricao: "Tatiana Weston-Webb é uma surfista profissional havaiana que representa o Brasil em competições internacionais. Conhecida por sua potência e estilo, ela é uma das principais atletas do surfe feminino mundial.",
+    link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
+}
 ];
