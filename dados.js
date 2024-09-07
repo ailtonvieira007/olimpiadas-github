1408dbbd5e0413099e59691bd3700b142e39309e
Ailton Vieira <91479065+ailtonvieira007@users.noreply.github.com>
2024-09-07 17:38:49 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -3,20 +3,24 @@ let dados = [
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
         link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        tags: "skate fada sk8"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza"
+        tags: "judô judo judoca judoka"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
         link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        tags: "ginástica ginastica ouro"
     },
     {
-    titulo: "Tatiana Weston-Webb",
-    descricao: "Tatiana Weston-Webb é uma surfista profissional havaiana que representa o Brasil em competições internacionais. Conhecida por sua potência e estilo, ela é uma das principais atletas do surfe feminino mundial.",
-    link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
-}
+        titulo: "Tatiana Weston-Webb",
+        descricao: "Tatiana Weston-Webb é uma surfista profissional havaiana que representa o Brasil em competições internacionais. Conhecida por sua potência e estilo, ela é uma das principais atletas do surfe feminino mundial.",
+        link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb"
+        tags: "surf praia onda"
+    }
 ];
